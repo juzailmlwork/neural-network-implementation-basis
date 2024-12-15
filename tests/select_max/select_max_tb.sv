0ff17836510bc21a6fff80df0f25801c56c266bf
juzailmlwork <juzailmlwork@gmail.com>
2024-12-16 01:37:41 +0530

    fixed testbench of select_max_tb

--- tests/select_max/select_max_tb.sv
+++ tests/select_max/select_max_tb.sv
@@ -12,13 +12,13 @@ module select_max_tb;
 logic signed [15:0] test_data_a [0:9] = {0,0,5,85,0,10,0,0,0,0};
 
 localparam period = 20;
-reg clk, enable, reset, done;  
+reg clk, enable, reset, layer_done;  
 reg signed [15:0] in_data [0:9];
 reg signed [15:0] max;
 reg [7:0] digit;
 
 select_max s_max(.clk(clk), .reset(reset), .enable(enable), .in_data(in_data),
-                 .digit(digit), .layer_done(done),.max(max));
+                 .digit(digit), .layer_done(layer_done),.max(max));
 
 initial begin
 clk = 0;
@@ -27,11 +27,11 @@ reset = 0;
 in_data = test_data_a;
 #145
 enable = 1;
-#2000
+#2500
 
-if (done) begin
-    if (max == 83) begin
-        $display("Maximum digit is : max = %d, expected 55 at time %t", max, $time);
+if (layer_done) begin
+    if (digit == 3) begin
+        $display("max index is :  %d", digit);
     end
 end
 
