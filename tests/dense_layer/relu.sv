4bdfa072b672610cbdf59c00d04fb847c84d9df7
juzailmlwork <juzailmlwork@gmail.com>
2024-12-15 16:37:24 +0530

    dense layer testbenches

---
+++ tests/dense_layer/relu.sv
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// Module Name: relu 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module relu #(parameter WIDTH = 8)(
+    input signed [4*WIDTH-1:0] data_in,
+    output signed [2*WIDTH-1:0] data_out
+    );
+    
+    wire signed [4*WIDTH-1:0] temp;
+    
+    assign temp = (data_in > 0)? data_in:0;
+    assign data_out = temp >> 8;
+    
+endmodule
